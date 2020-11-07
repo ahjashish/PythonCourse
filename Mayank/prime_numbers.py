95985267d47b3e2c03cc279291e915e7d4856801
mayank1978 <73350720+mayank1978@users.noreply.github.com>
2020-11-07 18:28:09 +0530

    Update prime_numbers.py

--- Mayank/prime_numbers.py
+++ Mayank/prime_numbers.py
@@ -3,8 +3,7 @@ num = int(input("enter the number"))
 for n in range(num+1):
   prime = True
   for m in range(2,n):
-    # print(f"{n,m,n/m}")
-    if n%m == 0:
+    if n % m == 0:
       print(f"{n}is not a prime number")
       prime = False
       break
